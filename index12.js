be22c577171da730c1cd23b332a94803983c4713
Muhammad Qasim <123858146+qasim-you@users.noreply.github.com>
2023-08-21 21:16:45 +0500

    Update index12.js

--- index12.js
+++ index12.js
@@ -3,7 +3,7 @@ Write a js program to input marks of five subjects Physics, Chemistry,
 Biology, Mathematics, and Computer. Calculate percentage and grade
 according to the following:
 Percentage >= 90% : Grade A
-Percentage >= 80% : Grade B
+Percentage >= 80% : Grade B 
 Percentage >= 70% : Grade C
 Percentage >= 60% : Grade D
 Percentage >= 40% : Grade E
